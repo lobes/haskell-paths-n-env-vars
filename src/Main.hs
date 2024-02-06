ed0acd48eafa3114cb44a572aefe7f168a85d268
lobes <lobes@duck.com>
2024-02-07 01:50:08 +1100

    Split it good

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Main.Utf8 qualified as Utf8
+import Data.Text (splitOn)
+import System.Directory (getCurrentDirectory)
 
 {- |
  Main entry point.
@@ -9,6 +10,6 @@ import Main.Utf8 qualified as Utf8
 -}
 main :: IO ()
 main = do
-  -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
-  Utf8.withUtf8 $ do
-    putTextLn "Hello 🌎"
+    currentDir <- getCurrentDirectory
+    let split = splitOn "/" $ toText currentDir
+    print split
